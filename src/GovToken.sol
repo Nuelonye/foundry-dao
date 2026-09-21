ad479039e15d1a657f01394e69134479e3386e78
nuel_onye <emmy17king@gmail.com>
2026-09-21 19:46:27 +0100

    remove unused import

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.24;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract GovToken is ERC20, EIP712, ERC20Votes {
